eb48db1e5180d78381e764e52b0c60a1ff7e9da3
EmmanuelCrocianelli <maguiteamoyoema@gmail.com>
2023-11-10 14:29:23 -0300

    login y registro terminados

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
+const { log } = require('console');
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
 });
+
+app.get('/crea_tu_cuenta', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/registro.html"))
+});
+
+app.post('/crea_tu_cuenta', (req, res) => {
+ console.log(req.body);
+ res.redirect("/")
+});
+
+app.get('/ingresa', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+});
+
+app.post('/ingresa', (req, res) => {
+    console.log(req.body);
+    res.redirect("/")
+   });
+
 
 app.use(express.static("public"));
 
